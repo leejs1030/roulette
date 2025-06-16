5dfb0e5642e78b0a2043c890e84d561ae68a951c
leejs1030 <leejs1030@korea.ac.kr>
2025-06-16 22:04:53 +0900

    Revert "회원가입 금지"
    This reverts commit df64a13c11d5f8b850e4d963ffd750d7abfd5be1.

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,13 +1,4 @@
-import {
-  Controller,
-  Post,
-  Body,
-  UseGuards,
-  Request,
-  SerializeOptions,
-  HttpException,
-  HttpStatus,
-} from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Request, SerializeOptions } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersService } from '../users/users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -25,11 +16,8 @@ export class AuthController {
   @Post('register')
   @SerializeOptions({ type: LoginResponseDto })
   async register(@Body() createUserDto: CreateUserDto): Promise<LoginResponseDto> {
-    // 서버 안정성을 위해 일시적으로 회원가입을 중단합니다
-    throw new HttpException('안정적인 운영을 위해 현재 회원가입을 일시 중단합니다.', HttpStatus.SERVICE_UNAVAILABLE);
-
-    // const user = await this.usersService.create(createUserDto);
-    // return this.authService.login(user);
+    const user = await this.usersService.create(createUserDto);
+    return this.authService.login(user);
   }
 
   @UseGuards(LocalAuthGuard)
